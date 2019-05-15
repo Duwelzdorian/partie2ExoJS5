f85febe51cf9f11320613723ce5f2facfb4fb564
Duwelzdorian <duwelzdorian@gmail.com>
2019-05-15 12:17:02 +0200

    exo5

---
+++ script.js
@@ -0,0 +1,6 @@
+function changeImage(element) {
+
+  /* (concaténer) récupère les attributs de l'élément pour le remplacer
+  par un nouveau chemin, pour une nouvelle image. */
+  element.src = 'images/' + element.id + '_2.jpg';
+}
